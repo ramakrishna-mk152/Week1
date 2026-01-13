d256738b2648ce430432dcb8f4f50e81c2d96719
Ramakrishna <ramakrishnamk2006@gmail.com>
2026-01-13 17:07:36 +0530

    Add files via upload

---
+++ Sort_Array_of_0_1_2.cpp
@@ -0,0 +1,16 @@
+//below code as executed in gfg
+class Solution {
+  public:
+    void sort012(vector<int>& arr) {
+        // code here
+        int low=0,mid=0;
+        int high=arr.size()-1;
+        while(mid<=high){
+            if(arr[mid]==0){
+                swap(arr[low++],arr[mid++]);
+            }
+            else if(arr[mid]==1) mid++;
+            else swap(arr[mid],arr[high--]);
+        }
+    }
+};
